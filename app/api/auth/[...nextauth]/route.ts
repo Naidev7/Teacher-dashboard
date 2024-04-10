f5afefca85a0389f668a567cd98aa9b068195753
Naidaly ruiz <naidalyruiz@gmail.com>
2024-04-10 12:16:32 +0200

    build demo

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
+
 export const authOptions = {
   providers: [
     CredentialsProvider({
