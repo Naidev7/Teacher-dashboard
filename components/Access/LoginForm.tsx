e2420dbd44c51405c6ddbf26465b5eaf4389d607
Naidaly ruiz <naidalyruiz@gmail.com>
2024-04-10 09:44:45 +0200

    regex in email and password

--- components/Access/LoginForm.tsx
+++ components/Access/LoginForm.tsx
@@ -23,7 +23,7 @@ export default function LoginForm() {
       });
 
       if (res.error) {
-        setError("Invalid Credentials");
+        setError("Credenciales inválidas");
         return;
       }
 
@@ -36,32 +36,32 @@ export default function LoginForm() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="shadow-lg space-y-16 p-12 rounded-lg border-t-4 border-indigo-400">
-        <h1 className="font-bold text-center text-2xl leading-6 text-gray-900 ">Login</h1>
+        <h1 className="font-bold text-center text-2xl leading-6 text-gray-900 ">Acceso</h1>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3 space-y-6">
           <input
             onChange={(e) => setEmail(e.target.value)}
-            type="text"
-            placeholder="Email"
+            type="email"
+            placeholder="susaBlanco@gmail.com"
             className="block w-full rounded-lg shadow-base p-4 border-2 border-gray-100   focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 font-normal text-sm leading-4 text-gray-600 cursor-pointer"
           />
           <input
             onChange={(e) => setPassword(e.target.value)}
             type="password"
-            placeholder="Password"
+            placeholder="Contraseña"
             className="block w-full rounded-lg shadow-base p-4 border-2 border-gray-100   focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 font-normal text-sm leading-4 text-gray-600 cursor-pointer"
           />
           <button className="bg-indigo-700 text-white p-4 m-auto rounded-lg w-[50%] font-medium text-sm leading-5 cursor-pointer">
-            Login
+            Entrar
           </button>
           {error && (
-            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+            <div className="text-red-500 text-center  text-sm py-1 px-3 rounded-md mt-2">
               {error}
             </div>
           )}
 
           <Link className="text-sm mt-3 text-right" href={"/pages/register"}>
-            Don't have an account? <span className="underline">Register</span>
+            No tienes cuenta? <span className="underline">Regístrate</span>
           </Link>
         </form>
       </div>
--- components/Access/RegisterForm.tsx
+++ components/Access/RegisterForm.tsx
@@ -62,40 +62,43 @@ export default function RegisterForm() {
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg space-y-16 p-12 rounded-lg border-t-4 border-indigo-400">
         <h1 className="font-bold text-center text-2xl leading-6 text-gray-900 ">
-          Register
+          Registro
         </h1>
 
         <form onSubmit={handleSubmit} className="flex flex-col gap-3 space-y-6">
           <input
             onChange={(e) => setName(e.target.value)}
             type="text"
-            placeholder="Full Name"
+            placeholder="Susana Blanco"
             className="block w-full rounded-lg shadow-base p-4 border-2 border-gray-100   focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 font-normal text-sm leading-4 text-gray-600 cursor-pointer"
           />
           <input
             onChange={(e) => setEmail(e.target.value)}
-            type="text"
-            placeholder="Email"
+            type="email"
+            pattern="^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+            placeholder="susaBlanco@gmail.com"
             className="block w-full rounded-lg shadow-base p-4 border-2 border-gray-100   focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 font-normal text-sm leading-4 text-gray-600 cursor-pointer"
           />
           <input
             onChange={(e) => setPassword(e.target.value)}
             type="password"
-            placeholder="Password"
+            placeholder="Contraseña"
+            pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
             className="block w-full rounded-lg shadow-base p-4 border-2 border-gray-100   focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 font-normal text-sm leading-4 text-gray-600 cursor-pointer"
           />
+           <p className="text-slate-500 text-xs text-center">La contraseña debe tener almenos 8 characters</p>
           <button className="bg-indigo-700 text-white p-4 m-auto rounded-lg w-[50%] font-medium text-sm leading-5 cursor-pointer">
-            Register
+            Registro
           </button>
 
           {error && (
-            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+            <div className="text-red-500 text-center text-sm py-1 px-3 rounded-md mt-2">
               {error}
             </div>
           )}
 
-          <Link className="text-sm mt-3 text-right" href={"/"}>
-            Already have an account? <span className="underline">Login</span>
+          <Link className="text-sm mt-3 text-center" href={"/"}>
+            Ya tienes cuenta? <span className="underline">Acceso</span>
           </Link>
         </form>
       </div>
