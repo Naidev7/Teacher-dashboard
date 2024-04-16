ec520982060cd580aa4de8419eebffcfa5111dee
Naidaly ruiz <naidalyruiz@gmail.com>
2024-04-16 14:50:50 +0200

    calendar add and delete funct

--- components/Dashboard/CalendarComp.tsx
+++ components/Dashboard/CalendarComp.tsx
@@ -1,18 +1,236 @@
 "use client";
-import { Calendar, dayjsLocalizer } from "react-big-calendar";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import dayjs from "dayjs";
+import "@/app/calendar.css";
+import FullCalendar from "@fullcalendar/react";
+import Menu from "./Menu";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin, {
+  Draggable,
+  DropArg,
+} from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { Fragment, useEffect, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CheckIcon, ExclamationTriangleIcon } from "@heroicons/react/20/solid";
+
+interface Event {
+  title: string;
+  start: Date | string;
+  allDay: boolean;
+  id: number;
+}
 
 function CalendarComp() {
-  const localizer = dayjsLocalizer(dayjs);
+  const [events, setEvents] = useState([
+    { title: "event 1", id: "1" },
+    { title: "event 2", id: "2" },
+    { title: "event 3", id: "3" },
+    { title: "event 4", id: "4" },
+    { title: "event 5", id: "5" },
+  ]);
+  const [allEvents, setAllEvents] = useState<Event[]>([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [idToDelete, setIdToDelete] = useState<number | null>(null);
+  const [newEvent, setNewEvent] = useState<Event>({
+    title: "",
+    start: "",
+    allDay: false,
+    id: 0,
+  });
 
+  useEffect(() => {
+    let draggableEl = document.getElementById("draggable-el");
+    if (draggableEl) {
+      new Draggable(draggableEl, {
+        itemSelector: ".fc-event",
+        eventData: function (eventEl) {
+          let title = eventEl.getAttribute("title");
+          let id = eventEl.getAttribute("data");
+          let start = eventEl.getAttribute("start");
+          return { title, id, start };
+        },
+      });
+    }
+  }, []);
+
+  //Create events
+  function handleDateClick(arg: { date: Date; allDay: boolean }) {
+    setNewEvent({
+      ...newEvent,
+      start: arg.date,
+      allDay: arg.allDay,
+      id: new Date().getTime(),
+    });
+    setShowModal(true);
+  }
+
+  //add event func
+  function addEvent(data: DropArg) {
+    console.log("Data: ", data);
+    const event = {
+      ...newEvent,
+      start: data.date.toISOString(),
+      title: data.draggedEl.innerText,
+      allDay: data.allDay,
+      id: new Date().getTime(),
+    };
+    setAllEvents([...allEvents, event]);
+  }
+
+  //delete modal
+  function handleDeleteModal(data: { event: { id: string } }) {
+    setShowDeleteModal(true);
+    setIdToDelete(Number(data.event.id));
+  };
+  // delete button
+  function handleDelete() {
+    setAllEvents(allEvents.filter(event => Number(event.id) !== Number(idToDelete)))
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
+  function handleCloseModal() {
+    setShowModal(false)
+    setNewEvent({
+      title: '',
+      start: '',
+      allDay: false,
+      id: 0
+    })
+    setShowDeleteModal(false)
+    setIdToDelete(null)
+  }
 
 
   return (
-    <div style={{height:"95vh",
-        width:"70vw"
-    }}>
-      <Calendar localizer={localizer} /* style={{ width: 800, height: 700 }} */ />
+    <div className="flex">
+      <span className="h-screen bg-white mx-w-[30%]">
+        <Menu />
+      </span>
+      <div className="w-[90%]">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="grid grid-cols-10">
+            <div className="col-span-8">
+              <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+                headerToolbar={{
+                  left: "prev,next today",
+                  center: "title",
+                  right: "resourceTimelineWook, dayGridMonth,timeGridWeek",
+                }}
+                events={allEvents}
+                nowIndicator={true}
+                editable={true}
+                droppable={true}
+                selectable={true}
+                selectMirror={true}
+                dateClick={{ handleDateClick }}
+                drop={(data) => addEvent(data)}
+                eventClick={(data) => handleDeleteModal(data)}
+              />
+            </div>
+            <div
+              id="draggable-el"
+              className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50"
+            >
+              <h1 className="font-bold text-lg text-center">Drag Event</h1>
+              {events.map((event) => (
+                <div
+                  className="fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
+                  title={event.title}
+                  key={event.id}
+                >
+                  {event.title}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>
+            <Transition.Root show={showDeleteModal} as={Fragment}>
+              <Dialog
+                as="div"
+                className="relative z-10"
+                onClose={setShowDeleteModal}
+              >
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0"
+                  enterTo="opacity-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                </Transition.Child>
+
+                <div className="fixed inset-0 z-10 overflow-y-auto">
+                  <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                      enterTo="opacity-100 translate-y-0 sm:scale-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                      leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    >
+                      <Dialog.Panel
+                        className="relative transform overflow-hidden rounded-lg
+                   bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+                      >
+                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                          <div className="sm:flex sm:items-start">
+                            <div
+                              className="mx-auto flex h-12 w-12 flex-shrink-0 items-center 
+                      justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+                            >
+                              <ExclamationTriangleIcon
+                                className="h-6 w-6 text-red-600"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                              <Dialog.Title
+                                as="h3"
+                                className="text-base font-semibold leading-6 text-gray-900"
+                              >
+                                Eliminar evento
+                              </Dialog.Title>
+                              <div className="mt-2">
+                                <p className="text-sm text-gray-500">
+                                  ¿Seguro que quieres eliminar este evento?
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                          <button
+                            type="button"
+                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm 
+                      font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                            onClick={handleDelete}
+                          >
+                            Eliminar
+                          </button>
+                          <button
+                            type="button"
+                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 
+                      shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                            onClick={handleCloseModal}
+                          >
+                            Cancelar
+                          </button>
+                        </div>
+                      </Dialog.Panel>
+                    </Transition.Child>
+                  </div>
+                </div>
+              </Dialog>
+            </Transition.Root>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
